0153addfd55fbb3048fcc9579ec1fd00bd672512
Luca Pacinotti <82702091+BBLLUUSSEE@users.noreply.github.com>
2022-05-06 15:17:41 +0200

    Update and rename Factory/SpesaFactory.cs to Fabbrica/SpesaFactory.cs

---
+++ Fabbrica/SpesaFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneSpese.Factory
+{
+    public class SpesaFactory
+    {
+        public ISpesa GetVehicle(string descrizionerimborso)
+        {
+            ISpesa spesa = null;
+            
+            return vehicle;
+        }
+
+    }
+}
